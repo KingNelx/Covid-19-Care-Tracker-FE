051fb19c970a2b822b35a6f7f47818e27de67baa
KingNelx <tapiajonel606@gmail.com>
2023-12-09 22:07:31 +0800

    added more animations

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,23 +1,28 @@
-import covid from '../Images/covid-logo.png'
 import Start from './Pages/Start';
 import Lottie from 'lottie-react';
+import shield from './Animations/shield.json'
 const Home = () => {
 
     return (
-        <div>
-            <div className="grid grid-col-2 grid-flow-col gap-4 text-center">
-                <div>
-                    <img src={covid} alt='Covid 19 Logo' />
+        <div className="flex flex-col items-center justify-center h-screen">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-center">
+                <div className="md:self-center">
+                    <Lottie
+                        animationData={shield}
+                        height={300}
+                        width={300}
+                        className="mx-auto"
+                    />
                 </div>
-                <div style={
-                    { marginTop: "38vh" }
-                } className="font-bold text-xl">
-                    Welcome to Covid 19 Care Tracker !
+                <div className="md:flex md:flex-col md:justify-center">
+                    <div className="font-bold text-3xl md:text-4xl mb-4">
+                        Welcome to Covid 19 Care Tracker!
+                    </div>
                 </div>
             </div>
-              <footer>
-              <Start />
-                  </footer>
+            <footer>
+                <Start />
+            </footer>
         </div>
     );
 }
